61674a4dcf0132bc856172e3e62b1e22e6534e21
Eduardo Rerick <eduardorerick@gmail.com>
2022-04-24 19:30:47 -0300

    added transition in login

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Container,
+  Grow,
   Stack,
   TextField,
   Typography,
@@ -10,64 +11,66 @@ import {
 export function Login() {
   return (
     <Container>
-      <Box
-        sx={{
-          height: "100vh",
-        }}
-      >
+      <Grow in={true} timeout={1000}>
         <Box
           sx={{
-            display: "flex",
             height: "100vh",
-            alignItems: "center",
-            justifyContent: "space-evenly",
           }}
         >
-          <Typography variant="h4">Seja bem-vindo!</Typography>
           <Box
             sx={{
-              borderRadius: "5px",
-              padding: "16px 32px",
-              border: "1px solid white",
+              display: "flex",
+              height: "100vh",
+              alignItems: "center",
+              justifyContent: "space-evenly",
             }}
           >
-            <Stack direction={"column"} spacing={4}>
-              <TextField
-                fullWidth
-                focused
-                variant="standard"
-                label={"Username"}
-                color={"secondary"}
-              />
-              <TextField
-                focused
-                variant="standard"
-                fullWidth
-                inputProps={{ type: "password" }}
-                label={"Senha"}
-                color={"secondary"}
-              />
-            </Stack>
+            <Typography variant="h4">Seja bem-vindo!</Typography>
             <Box
               sx={{
-                marginTop: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: 1,
-                justifyContent: "center",
+                borderRadius: "5px",
+                padding: "16px 32px",
+                border: "1px solid white",
               }}
             >
-              <Typography>
-                Ainda não tem acesso? Faça o seu cadastro!
-              </Typography>
-              <Button variant={"contained"}>
-                <Typography>Cadastrar</Typography>
-              </Button>
+              <Stack direction={"column"} spacing={4}>
+                <TextField
+                  fullWidth
+                  focused
+                  variant="standard"
+                  label={"Username"}
+                  color={"secondary"}
+                />
+                <TextField
+                  focused
+                  variant="standard"
+                  fullWidth
+                  inputProps={{ type: "password" }}
+                  label={"Senha"}
+                  color={"secondary"}
+                />
+              </Stack>
+              <Box
+                sx={{
+                  marginTop: 4,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: 1,
+                  justifyContent: "center",
+                }}
+              >
+                <Typography>
+                  Ainda não tem acesso? Faça o seu cadastro!
+                </Typography>
+                <Button variant={"contained"}>
+                  <Typography>Cadastrar</Typography>
+                </Button>
+              </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
+      </Grow>
     </Container>
   );
 }
