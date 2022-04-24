a27768e844ccec0a3a532dd3a1bec9df10e15618
Eduardo Rerick <eduardorerick@gmail.com>
2022-04-24 19:12:23 -0300

    added cors to api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
