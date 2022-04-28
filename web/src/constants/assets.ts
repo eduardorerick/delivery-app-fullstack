a0438b7f5a2f46e882ddc2d22446364319e74a95
Eduardo Rerick <eduardorerick@gmail.com>
2022-04-28 20:19:46 -0300

    added assets constants

---
+++ web/src/constants/assets.ts
@@ -0,0 +1,4 @@
+export enum AssetPath {
+  LANDING_PAGE_BIKE_DELIVERY = '/assets/landingPage/bikeDelivery.svg',
+  LANDING_PAGE_WAVE = '/assets/landingPage/wave.svg'
+}
--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -1,42 +1,89 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { RouterPath } from "../constants/routes";
+import { AssetPath, RouterPath } from "../constants";
 
 export function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <Box component="nav" sx={{ height: "100vh", marginTop: 1 }}>
-        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography variant="h6" sx={{ userSelect: 'none', color: "primary.main" }}>
-            Delivery App
-          </Typography>
-          <Box>
-            <Button
-              variant="text"
-              onClick={() => navigate(RouterPath.CLIENT_LOGIN)}
-              sx={{ color: "primary.main", textTransform: "none" }}
+    <Box
+      sx={{
+        width: "100%",
+        backgroundSize: "cover",
+        height: "64vh",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundImage: `url(${AssetPath.LANDING_PAGE_WAVE})`,
+      }}
+    >
+      <Container>
+        <Box sx={{ height: "100vh", paddingTop: 1 }}>
+          <Box component="nav">
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography
+                variant="h6"
+                sx={{ userSelect: "none", color: "primary.contrastText" }}
+              >
+                Delivery App
+              </Typography>
+              <Box>
+                <Button
+                  variant="text"
+                  onClick={() => navigate(RouterPath.CLIENT_LOGIN)}
+                  sx={{ color: "primary.contrastText", textTransform: "none" }}
+                >
+                  <Typography>Login como cliente</Typography>
+                </Button>
+                <Button
+                  variant="text"
+                  onClick={() => navigate("/login")}
+                  sx={{ color: "primary.contrastText", textTransform: "none" }}
+                >
+                  <Typography>Login como motorista</Typography>
+                </Button>
+                <Button
+                  variant="text"
+                  onClick={() => navigate("/login")}
+                  sx={{ color: "primary.contrastText", textTransform: "none" }}
+                >
+                  <Typography>Login como restaurante</Typography>
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              userSelect: "none",
+              display: "flex",
+              gap: "40px",
+              height: "80%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              sx={{
+                flex: 1,
+              }}
             >
-              <Typography>Login como cliente</Typography>
-            </Button>
-            <Button
-              variant="text"
-              onClick={() => navigate("/login")}
-              sx={{ color: "primary.main", textTransform: "none" }}
-            >
-              <Typography>Login como motorista</Typography>
-            </Button>
-            <Button
-              variant="text"
-              onClick={() => navigate("/login")}
-              sx={{ color: "primary.main", textTransform: "none" }}
-            >
-              <Typography>Login como restaurante</Typography>
-            </Button>
+              <img
+                style={{ width: "100%" }}
+                src={AssetPath.LANDING_PAGE_BIKE_DELIVERY}
+                alt="Woman in bike making a delivery"
+              />
+            </Box>
+            <Box sx={{ flex: 1, color: "primary.contrastText" }}>
+              <Typography variant="h3">Delivery App</Typography>
+              <Typography variant="body1">
+                Melhor app fullstack de entregas
+              </Typography>
+              <Typography variant="body2">
+                Feito para estudar React, Docker, Express e Postgres!
+              </Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 }
